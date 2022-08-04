e7412b8936b342983f9f0ebd4e3fdb12bc8caddc
TomohiroAoki <a12tomo13@outlook.com>
2022-08-04 01:45:17 +0000

    add test

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 Serial pc(USBTX, USBRX, 115200);
 DigitalOut LED_0(PA_0);
 PwmOut LED_1(PA_6);
- 
+
 //Nucleo f303k8用
 USB Usb(D11, D12, D13, A3, A2); // mosi, miso, sclk, ssel, intr
 BTD Btd(&Usb);
@@ -110,3 +110,38 @@ int main(){
         }
     }
 }
+
+
+//PS3 controller test
+#if 0
+int main(){
+    if (Usb.Init() == -1){
+        pc.printf("\r\nOSC did not start");
+        while (1); // Halt
+    }
+    
+    pc.printf("\r\nPS3 USB Library Started");
+ 
+    while (1){
+        Usb.Task();
+        
+        if (PS3.PS3Connected) {
+            printf("\n\r%3d %3d %3d %3d",PS3.getAnalogHat(LeftHatX),PS3.getAnalogHat(LeftHatY),PS3.getAnalogHat(RightHatX),PS3.getAnalogHat(RightHatY));
+            printf(" %3d %3d %3d %3d",PS3.getAnalogButton(UP),PS3.getAnalogButton(DOWN),PS3.getAnalogButton(RIGHT),PS3.getAnalogButton(LEFT));
+            printf(" %3d %3d %3d %3d",PS3.getAnalogButton(TRIANGLE),PS3.getAnalogButton(CIRCLE),PS3.getAnalogButton(CROSS),PS3.getAnalogButton(SQUARE));
+            printf(" %3d %3d %3d %3d",PS3.getAnalogButton(L1),PS3.getAnalogButton(R1),PS3.getAnalogButton(L2),PS3.getAnalogButton(R2));
+            //printf(" %3d %3d",PS3.getAnalogButton(SELECT),PS3.getAnalogButton(START));
+            //printf(" %3d %3d",PS3.getAnalogButton(L3),PS3.getAnalogButton(R3));
+            if(PS3.getAnalogButton(CROSS) > 50){
+                led = 1;
+            }else{
+                led = 0;
+            }
+            
+            
+        }else{
+            pc.printf("not connect\n");
+        }
+    }
+}
+#endif
